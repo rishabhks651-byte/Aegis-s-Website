48388d31751775d46f0da5d038fee6a71c870ad1
si1entkerne1 <rishabhks651@gmail.com>
2026-07-20 21:20:07 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/Aegis-s-Website",
+  assetPrefix: "/Aegis-s-Website/",
 };
 
 export default nextConfig;
